1982a72d8f361b8411a7650f1abd1bfca1cd815d
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-11-05 11:00:47 +0600

    feat: add navbar functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import BuyCredit from "./pages/BuyCredit";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
 
 const App = () => {
    return (
-      <div>
+      <div className="min-h-screen bg-slate-50">
+         <Navbar />
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/result" element={<Result />} />
